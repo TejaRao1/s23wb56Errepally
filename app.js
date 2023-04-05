7ac0714a14adbacc8841c3f2f6718f105a0eba1e
TejaRao1 <ravitejaeravelli.re@gmail.com>
2023-04-05 17:24:09 -0500

    changes in app.js

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var class03Router = require('./routes/class03');
+var BoardRouter = require('./routes/Board');
+var SelectorRouter = require('./routes/Selector');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/class03', class03Router);
+app.use('/Board', BoardRouter);
+app.use('/Selector', SelectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
