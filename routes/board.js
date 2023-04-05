6cdb2effbb579fceaa1a1d798695608b37196059
TejaRao1 <ravitejaeravelli.re@gmail.com>
2023-04-05 17:38:11 -0500

    changes in board

--- routes/board.js
+++ routes/board.js
@@ -4,9 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let query = req.query
-  console.log(`rows ${query.rows}`)
-  console.log(`cols ${query.cols}`)
-  res.render('Board', { title: 'Board Display' , query: query });
+  res.render('board', { title: 'Board Display' , query: query });
 });
 
 module.exports = router;
